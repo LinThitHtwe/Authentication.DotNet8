7eec34fca8583b0bd5045470bae35a67dff6973c
LinThitHtwe <linthit2745@gmail.com>
2024-07-05 22:09:33 +0630

    [add] Authorization Example

---
+++ DotNet8Authentication.Api/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8Authentication.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet,Authorize]
+        public IActionResult Test()
+        {
+            return Ok("Authorize Request");
+        }
+    }
+}
--- DotNet8Authentication.Api/Program.cs
+++ DotNet8Authentication.Api/Program.cs
@@ -1,6 +1,8 @@
 using DotNet8Authentication.Api.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    }
+    );
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 
 builder.Services.AddDbContext<AppDbContext>(context =>
 {
